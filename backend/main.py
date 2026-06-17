8019d173256fff5e9f22968762ac69d7eba5235b
Arshia Fajar <etifah2124@gmail.com>
2026-06-17 23:25:10 +0500

    CORS wildcard for all Vercel previews

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
